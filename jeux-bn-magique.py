bb81e078b1191867886bb43618346f585a0af0c5
Mahamadou Cisse <mahamadoucisse2020@icloud.com>
2026-01-23 11:20:29 +0100

    Jeux du nombre magique

---
+++ jeux-bn-magique.py
@@ -0,0 +1,66 @@
+#1 CREATION DES VARIABLES
+#2 CREATION DE LA FONCTION : def/input
+#3 AJOUT DE LA CONDITION  : if, elif, else
+#4 AJOUT DE LA BOUVLE : while 
+#5 AJOUT LE CAS D'ERREUR : try/except
+#6 AJOUT LE NOMBRE ALEATOIR : random
+#6 AJOUT LE NOMBRE DE LIMITER 
+
+import random
+
+NOMBRE_MIN = 1
+NOMBRE_MAX = 10
+NOMBRE_MAGIQUE = random.randint(NOMBRE_MIN, NOMBRE_MAX)
+NB_VIE = 4
+
+
+def demander_nombre(nb_min, nb_max):
+    nombre_int = 0
+    while nombre_int == 0:
+        nombre_str = input(f"Quel est le nombre magique entre ({nb_min} et {nb_max})? : ")
+        try:
+            nombre_int = int(nombre_str)
+        except:
+            print("ERROR: vous devez un nombre. Réessayez")
+        else:
+            if nombre_int < nb_min or nombre_int > nb_max:
+                print(f"ERROR: le nombre magique doit etre entre ({nb_min} et {nb_max}). Réessayez")
+                nombre_int = 0
+    return nombre_int
+
+
+    # AVEC LA BOUCLE WHILE
+# nombre = 0
+# vie = NB_VIE
+# while not nombre == NOMBRE_MAGIQUE and vie > 0:
+#     print(f"il vous restez {vie} vies")
+#     nombre = demander_nombre(NOMBRE_MIN, NOMBRE_MAX)
+#     if nombre == NOMBRE_MAGIQUE:
+#         print("Bravo vous avez gagne")
+#     elif nombre > NOMBRE_MAGIQUE:
+#         print("le nombre magique est petit")
+#
+#     else:
+#         print("le nombre magique est grand")
+#
+# if vie == 0:      
+#     print(f"Vous avez perdu le nombre magique etait {NOMBRE_MAGIQUE}")
+
+
+        # AVEC LA BOUCLE FOR
+gagne = False
+for i in range(0, NB_VIE):
+    vie = NB_VIE-i
+    print(f"il vous restez {vie} vies")
+    nombre = demander_nombre(NOMBRE_MIN, NOMBRE_MAX)
+    if nombre == NOMBRE_MAGIQUE:
+        print("Bravo vous avez gagne")
+        gagne = True
+        break
+    elif nombre > NOMBRE_MAGIQUE:
+        print("le nombre magique est petit")
+    else:
+        print("le nombre magique est grand")
+ 
+if gagne == 0:
+    print(f"Vous avez perdu le nombre magique etait {NOMBRE_MAGIQUE}")
--- main-fct.py
+++ main-fct.py
@@ -16,32 +16,34 @@ def demander_nom():
 # 2 fct demander l'age
 def demander_age():
     age = 0
-
     while age == 0:
         age_str = input("Quel est votre age ? : ")
         try:
             age = int(age_str)
         except:
             print("ERREUR: vous devez entre un nombre pour l'age")
-        return age
+    return age
         
 
 def afficher_informations_personne(nom, age):
     print(f"Vous vous appelez " + nom + ", vous avez " + str(age) + " ans")
     print("L'annee prochaine vous aurez " + str(age + 1) + " ans")  
 
-nom1 = demander_nom()
-nom2 = demander_nom()
+# nom1 = demander_nom()
+# nom2 = demander_nom()
 
 
-age1 = demander_age()
-age2 = demander_age()
+# age1 = demander_age()
+# age2 = demander_age()
 
-# Afficher
-# fct afficher informations personne
+# # Afficher
+# # fct afficher informations personne
 
-afficher_informations_personne(nom1, age1)
-afficher_informations_personne(nom2, age2)
+# afficher_informations_personne(nom1, age1)
+# afficher_informations_personne(nom2, age2)
 
 
-
+for i in range(0,2):
+    nom = demander_nom()
+    age = demander_age()
+    afficher_informations_personne(nom, age)
